cf341e4f9d6476a48b69e59a03f793a2889909ba
denis.bredun <bredun.denis@gmail.com>
2025-01-09 16:17:51 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FutureWeather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FutureWeather
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
