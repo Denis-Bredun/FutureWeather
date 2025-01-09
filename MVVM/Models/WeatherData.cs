2a9ffbcc5ca626d52e71522a06c367356ce7f8e8
denis.bredun <bredun.denis@gmail.com>
2025-01-09 19:30:03 +0200

    Added models + made logic for ViewModel

---
+++ MVVM/Models/WeatherData.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureWeather.MVVM.Models
+{
+
+    public class WeatherData
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public float generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public float elevation { get; set; }
+        public Current_Units current_units { get; set; }
+        public Current current { get; set; }
+        public Daily_Units daily_units { get; set; }
+        public Daily daily { get; set; }
+    }
+
+    public class Current_Units
+    {
+        public string time { get; set; }
+        public string interval { get; set; }
+        public string temperature_2m { get; set; }
+        public string relative_humidity_2m { get; set; }
+        public string apparent_temperature { get; set; }
+        public string is_day { get; set; }
+        public string precipitation { get; set; }
+        public string rain { get; set; }
+        public string showers { get; set; }
+        public string snowfall { get; set; }
+        public string weather_code { get; set; }
+        public string cloud_cover { get; set; }
+        public string pressure_msl { get; set; }
+        public string surface_pressure { get; set; }
+        public string wind_speed_10m { get; set; }
+        public string wind_direction_10m { get; set; }
+        public string wind_gusts_10m { get; set; }
+    }
+
+    public class Current
+    {
+        public string time { get; set; }
+        public int interval { get; set; }
+        public float temperature_2m { get; set; }
+        public int relative_humidity_2m { get; set; }
+        public float apparent_temperature { get; set; }
+        public int is_day { get; set; }
+        public float precipitation { get; set; }
+        public float rain { get; set; }
+        public float showers { get; set; }
+        public float snowfall { get; set; }
+        public int weather_code { get; set; }
+        public int cloud_cover { get; set; }
+        public float pressure_msl { get; set; }
+        public float surface_pressure { get; set; }
+        public float wind_speed_10m { get; set; }
+        public int wind_direction_10m { get; set; }
+        public float wind_gusts_10m { get; set; }
+    }
+
+    public class Daily_Units
+    {
+        public string time { get; set; }
+        public string weather_code { get; set; }
+        public string temperature_2m_max { get; set; }
+        public string temperature_2m_min { get; set; }
+    }
+
+    public class Daily
+    {
+        public string[] time { get; set; }
+        public int[] weather_code { get; set; }
+        public float[] temperature_2m_max { get; set; }
+        public float[] temperature_2m_min { get; set; }
+    }
+
+}
--- MVVM/ViewModels/WeatherViewModel.cs
+++ MVVM/ViewModels/WeatherViewModel.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using FutureWeather.MVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FutureWeather.MVVM.ViewModels
 {
-    internal class WeatherViewModel
+    public class WeatherViewModel
     {
+        public WeatherData WeatherData { get; set; }
+        private HttpClient _httpClient;
+
+        public WeatherViewModel()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        private async Task<Location> GetCoordinatesAsync(string address)
+        {
+            var locations = await Geocoding.Default.GetLocationsAsync(address);
+
+            var location = locations?.FirstOrDefault();
+
+            if (location != null)
+                Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+
+            return location;
+        }
+
+        public ICommand SearchCommand => new Command(async (searchText) =>
+        {
+            var location = await GetCoordinatesAsync(searchText.ToString());
+            await GetWeather(location);
+        });
+
+        private async Task GetWeather(Location location)
+        {
+            var latitude = location.Latitude.ToString(CultureInfo.InvariantCulture);
+            var longitude = location.Longitude.ToString(CultureInfo.InvariantCulture);
+
+            var url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Africa%2FCairo";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                using (var responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    var data = await JsonSerializer.DeserializeAsync<WeatherData>(responseStream);
+                    WeatherData = data;
+                }
+            }
+        }
     }
 }
